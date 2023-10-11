06e83518e7490632c811f10d6b723410f7d47763
HyungJoon <itineris24@naver.com>
2023-10-11 14:54:05 -0700

    linux text

--- src/UnorderedSet.cpp
+++ src/UnorderedSet.cpp
@@ -3,8 +3,106 @@
 
 
 template<typename Key>
-UnorderedSet::Iterator UnorderedSet<Key>::begin() const {
-    return UnorderedSet::Iterator(nullptr);
+UnorderedSet<Key>::UnorderedSet() {
+
 }
 
+template<typename Key>
+UnorderedSet<Key>::~UnorderedSet() {
+
+};
+
+
+template <typename Key>
+typename UnorderedSet<Key>::Iterator UnorderedSet<Key>::begin() const {
+    return Iterator(root);
+}
+
+
+template <typename Key>
+typename UnorderedSet<Key>::Iterator UnorderedSet<Key>::end() const {
+    return Iterator(nullptr);
+}
+
+
+template<typename Key>
+bool UnorderedSet<Key>::insert(const Key &key) {
+    return false;
+}
+
+
+template<typename Key>
+bool UnorderedSet<Key>::search(const Key &key) const {
+    bool result = false;
+
+    if (Iterator(root) != NULL || Iterator(root) == key) return true;
+
+    return false;
+}
+
+
+template<typename Key>
+bool UnorderedSet<Key>::erase(const Key &key) {
+    return false;
+}
+
+
+template<typename Key>
+void UnorderedSet<Key>::clear() {
+
+}
+
+
+template<typename Key>
+size_t UnorderedSet<Key>::size() const {
+    return 0;
+}
+
+
+template<typename Key>
+void UnorderedSet<Key>::updateSize() {
+
+}
+
+
+template<typename Key>
+size_t UnorderedSet<Key>::getSize(Node<Key> *node) const {
+    return setSize;
+}
+
+
+template<typename Key>
+void UnorderedSet<Key>::fixRedRedViolation(Node<Key> *node) {
+
+}
+
+
+template<typename Key>
+void UnorderedSet<Key>::rotateLeft(Node<Key> *node) {
+
+}
+
+
+template<typename Key>
+void UnorderedSet<Key>::rotateRight(Node<Key> *node) {
+
+}
+
+
+template<typename Key>
+void UnorderedSet<Key>::deleteOneChild(Node<Key> *node) {
+
+}
+
+
+template<typename Key>
+void UnorderedSet<Key>::deleteFix(Node<Key> *node) {
+
+}
+
+
+template<typename Key>
+void UnorderedSet<Key>::clearRecursive(Node<Key> *node) {
+
+}
 
