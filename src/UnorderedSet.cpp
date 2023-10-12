9aaeff60a095a402dad30f610a98bb9a82ef076c
HyungJoon <itineris24@naver.com>
2023-10-12 00:26:12 -0700

    erase should be fixed

--- src/UnorderedSet.cpp
+++ src/UnorderedSet.cpp
@@ -5,6 +5,7 @@
 template<typename Key>
 UnorderedSet<Key>::UnorderedSet() {
     setSize = 0;
+    root = 0;
 }
 
 template<typename Key>
@@ -15,24 +16,27 @@ UnorderedSet<Key>::~UnorderedSet() {
 
 template <typename Key>
 typename UnorderedSet<Key>::Iterator UnorderedSet<Key>::begin() const {
-    Node<Key>* node = root;
-    while (node && node->left) {
-        node = node->left;
-    }
-    return Iterator(node);
+    return Iterator(root);
 }
 
 
 template <typename Key>
 typename UnorderedSet<Key>::Iterator UnorderedSet<Key>::end() const {
-    return Iterator(nullptr);
+    Node<Key>* temp;
+
+    temp = root;
+    while(temp->right) {
+        temp = temp->right;
+    }
+
+    if (temp->left) return Iterator(temp->left);
+    else return Iterator(temp);
 }
 
 
 template<typename Key>
 bool UnorderedSet<Key>::insert(const Key &key) {
-    // This is a simplified insertion without handling Red-Black Tree properties
-    Node<Key>* newNode = new Node<Key>(key);
+    auto* newNode = new Node<Key>(key);
     if (!root) {
         root = newNode;
         setSize++;
@@ -44,30 +48,22 @@ bool UnorderedSet<Key>::insert(const Key &key) {
 
     while (current) {
         parent = current;
-        if (key < current->key) {
-            current = current->left;
-        } else if (key > current->key) {
-            current = current->right;
-        } else {
-            // Duplicate key, not inserting
+        if (key < current->key) current = current->left;
+        else if (key > current->key) current = current->right;
+        else {
+            // Same key (Duplicated), free memory
             delete newNode;
             return false;
         }
     }
 
-    if (key < parent->key) {
-        parent->left = newNode;
-    } else {
-        parent->right = newNode;
-    }
+    if (key < parent->key) parent->left = newNode;
+    else parent->right = newNode;
     newNode->parent = parent;
+
     setSize++;
 
-    // TODO: Handle Red-Black Tree properties after insertion
-    // fixRedRedViolation(newNode);
-
     return true;
-
 }
 
 
@@ -75,45 +71,48 @@ template<typename Key>
 bool UnorderedSet<Key>::search(const Key &key) const {
     Node<Key>* current = root;
     while (current) {
-        if (key < current->key) {
-            current = current->left;
-        } else if (key > current->key) {
-            current = current->right;
-        } else {
-            // Key found
-            return true;
-        }
+        if (key < current->key) current = current->left;
+        else if (key > current->key) current = current->right;
+        else return true;  // FOUND
     }
-    return false; // Key not found
-
+    return false;
 }
 
 
 template<typename Key>
 bool UnorderedSet<Key>::erase(const Key &key) {
-    // This is a simplified deletion without handling Red-Black Tree properties
     Node<Key>* current = root;
+
     while (current) {
-        if (key < current->key) {
-            current = current->left;
-        } else if (key > current->key) {
-            current = current->right;
-        } else {
-            // Key found, delete the node
-            if (!current->left || !current->right) {
+        if (key < current->key) current = current->left;
+        else if (key > current->key) current = current->right;
+        else {
+            if (!current->left) {
                 // Node with only one child or no child
                 deleteOneChild(current);
-            } else {
+                if (!(*current)) {
+                    current = current->left;
+                }
+            }
+            else if (!current->right) {
+                deleteOneChild(current);
+                if (!(*current)) {
+                    current = current->right;
+                }
+            }
+            else {
                 // Node with two children
-                Node<Key>* successor = Iterator(current).operator++().currentNode;
-                current->key = successor->key;
-                deleteOneChild(successor);
+                if (current == (*current).left) {
+                    (*current).right = current->right;
+                    (*current).left = current->left;
+                    deleteOneChild(current);
+                }
             }
             setSize--;
             return true;
         }
     }
-    return false; // Key not found
+    return false;
 }
 
 
@@ -163,7 +162,7 @@ void UnorderedSet<Key>::rotateRight(Node<Key> *node) {
 
 template<typename Key>
 void UnorderedSet<Key>::deleteOneChild(Node<Key> *node) {
-
+    free(node);
 }
 
 
