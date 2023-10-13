61bfcc2b6a6f34fcf67665fbe30a994ce02fc856
HyungJoon <itineris24@naver.com>
2023-10-13 06:36:11 -0700

    setsize()

--- src/UnorderedSet.cpp
+++ src/UnorderedSet.cpp
@@ -34,34 +34,42 @@ typename UnorderedSet<Key>::Iterator UnorderedSet<Key>::end() const {
 }
 
 
+// TODO:
 template<typename Key>
 bool UnorderedSet<Key>::insert(const Key &key) {
+    Node<Key>* cNode = root;
+    Node<Key>* pNode = nullptr;
     auto* newNode = new Node<Key>(key);
+    newNode->color = Color::RED;
+
     if (!root) {
         root = newNode;
+        root->color = Color::BLACK;
         setSize++;
         return true;
     }
 
-    Node<Key>* current = root;
-    Node<Key>* parent = nullptr;
-
-    while (current) {
-        parent = current;
-        if (key < current->key) current = current->left;
-        else if (key > current->key) current = current->right;
+    while (cNode) {
+        pNode = cNode;
+        if (key < cNode->key) cNode = cNode->left;
+        else if (key > cNode->key) cNode = cNode->right;
         else {
-            // Same key (Duplicated), free memory
+            // Key exist in the tree - free memory
             delete newNode;
             return false;
         }
     }
 
-    if (key < parent->key) parent->left = newNode;
-    else parent->right = newNode;
-    newNode->parent = parent;
-
+    if (key < pNode->key) pNode->left = newNode;
+    else pNode->right = newNode;
+    newNode->parent = pNode;
     setSize++;
+
+//
+//    while(root) {
+//        gets
+//    }
+//
 
     return true;
 }
@@ -83,35 +91,35 @@ template<typename Key>
 bool UnorderedSet<Key>::erase(const Key &key) {
     Node<Key>* current = root;
 
-    while (current) {
-        if (key < current->key) current = current->left;
-        else if (key > current->key) current = current->right;
-        else {
-            if (!current->left) {
-                // Node with only one child or no child
-                deleteOneChild(current);
-                if (!(*current)) {
-                    current = current->left;
-                }
-            }
-            else if (!current->right) {
-                deleteOneChild(current);
-                if (!(*current)) {
-                    current = current->right;
-                }
-            }
-            else {
-                // Node with two children
-                if (current == (*current).left) {
-                    (*current).right = current->right;
-                    (*current).left = current->left;
-                    deleteOneChild(current);
-                }
-            }
-            setSize--;
-            return true;
-        }
-    }
+//    while (current) {
+//        if (key < current->key) current = current->left;
+//        else if (key > current->key) current = current->right;
+//        else {
+//            if (!current->left) {
+//                // Node with only one child or no child
+//                deleteOneChild(current);
+//                if (!(*current)) {
+//                    current = current->left;
+//                }
+//            }
+//            else if (!current->right) {
+//                deleteOneChild(current);
+//                if (!(*current)) {
+//                    current = current->right;
+//                }
+//            }
+//            else {
+//                // Node with two children
+//                if (current == (*current).left) {
+//                    (*current).right = current->right;
+//                    (*current).left = current->left;
+//                    deleteOneChild(current);
+//                }
+//            }
+//            setSize--;
+//            return true;
+//        }
+//    }
     return false;
 }
 
@@ -126,7 +134,7 @@ void UnorderedSet<Key>::clear() {
 
 template<typename Key>
 size_t UnorderedSet<Key>::size() const {
-    return setSize;
+    return getSize(root);
 }
 
 
@@ -136,9 +144,13 @@ void UnorderedSet<Key>::updateSize() {
 }
 
 
+//Recursively calculates the size of the subtree rooted at the given node:
 template<typename Key>
 size_t UnorderedSet<Key>::getSize(Node<Key> *node) const {
-    return setSize;
+    if (node == nullptr)
+        return 0;
+    else
+        return(getSize(node->left) + 1 + getSize(node->right));
 }
 
 
